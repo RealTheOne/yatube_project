e7278ece07ed9fd1ec6393d5f2ed979779cfd5d0
Artur Idrisov <starlegionmail@gmail.com>
2022-11-20 00:51:09 +0500

    Added urls and view-functions

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+#from django.shortcuts import render (ORIGINAL IMPORT. WHAT THE HELL IS THAT?!)
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Главная страница. A small step for a...')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Here should be something about "group posts" {slug}')
