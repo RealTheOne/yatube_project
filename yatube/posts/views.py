44f945dc14cb43b4d8c07fdf8567de5935a747af
Artur Idrisov <starlegionmail@gmail.com>
2022-11-28 01:02:41 +0500

    Done requirments for grading

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,28 @@
-from django.shortcuts import render
-#from django.http import HttpResponse 
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 # Create your views here.
-
 def index(request):
-    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
     main_header = 'Это главная страница проекта Yatube'
+    title = 'Последние обновления на сайте'
     context = {
-        'header': main_header
+        'posts': posts,
+        'header': main_header,
+        'title': title
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
+
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    main_header = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    main_header = 'Лев Толстой - зеркало русской революции.'
+    title = 'Записи сообщества'
     context = {
-        'header': main_header
+        'title': title,
+        'header': main_header,
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
